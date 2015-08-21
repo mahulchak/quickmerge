519307653171f8bda1406f20d7ff660fd3f2c1b7
jgbaldwinbrown <jbaldwi1@uci.edu>
2015-08-21 14:49:23 -0700

    updated merge_wrapper.py for release

--- merge_wrapper.py
+++ merge_wrapper.py
@@ -10,8 +10,8 @@ parser = argparse.ArgumentParser(description = 'run mummer and the merge program
 parser.add_argument("hybrid_assembly_fasta", help="the output of a hybrid assembly program such as DBG2OLC")
 parser.add_argument("self_assembly_fasta",help="the output of a self assembly program such as PBcR")
 parser.add_argument("-pre","--prefix", help="the prefix for all output files")
-parser.add_argument("-p1","--param1", help="the first parameter for the merge program")
-parser.add_argument("-p2","--param2", help="the second parameter for the merge program")
+parser.add_argument("-hco","--hco", help="the quickmerge hco parameter")
+parser.add_argument("-c","--c", help="the quickmerge c parameter")
 parser.add_argument("--no_nucmer", help="skip the nucmer step",action="store_true")
 parser.add_argument("--no_delta", help="skip the nucmer and delta-filter steps",action="store_true")
 parser.add_argument("--stop_after_nucmer", help="do not perform the delta-filter and merger steps",action="store_true")
@@ -24,14 +24,14 @@ else:
   prefix = "out"
 hypath = args.hybrid_assembly_fasta
 selfpath = args.self_assembly_fasta
-if args.param1:
-  param1 = args.param1
-if args.param2:
-  param2 = args.param2
+if args.hco:
+  hco = args.hco
+if args.c:
+  c = args.c
 
 #run nucmer:
 if not args.no_nucmer and not args.no_delta:
-  subprocess.call(['nucmer','-l','100','-prefix',str(myprefix),str(selfpath),str(hypath)])
+  subprocess.call(['nucmer','-l','100','-prefix',str(prefix),str(selfpath),str(hypath)])
 
 #run the delta filter on the nucmer alignment:
 if not args.no_delta and not args.stop_after_nucmer:
@@ -39,13 +39,13 @@ if not args.no_delta and not args.stop_after_nucmer:
     subprocess.call(['delta-filter','-i','95',str(str(prefix)+'.delta')],stdout=outfile)
 
 #append the correct options to the merger call:
-mergercall = ['merger',str(str(prefix)+'.rq.delta'),str(hypath),str(selfpath)]
-if args.param1:
-  mergercall.append('-p1')
-  mergercall.append(str(param1))
-if args.param2:
-  mergercall.append('-p2')
-  mergercall.append(str(param2))
+mergercall = ['quickmerge','-d',str(str(prefix)+'.rq.delta'),'-q',str(hypath),'-r',str(selfpath)]
+if args.hco:
+  mergercall.append('-hco')
+  mergercall.append(str(hco))
+if args.c:
+  mergercall.append('-c')
+  mergercall.append(str(c))
 
 #run the merging program
 if not args.stop_after_nucmer and not args.stop_after_df:
