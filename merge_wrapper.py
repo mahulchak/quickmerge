abfdff8516e56e83a705844646efe97fdc065e4d
jgbaldwinbrown <jbaldwi1@uci.edu>
2015-09-10 13:32:20 -0700

    modified merge_wrapper to have defaults for hco, c, and l

--- merge_wrapper.py
+++ merge_wrapper.py
@@ -10,9 +10,9 @@ parser = argparse.ArgumentParser(description = 'run mummer and the merge program
 parser.add_argument("hybrid_assembly_fasta", help="the output of a hybrid assembly program such as DBG2OLC")
 parser.add_argument("self_assembly_fasta",help="the output of a self assembly program such as PBcR")
 parser.add_argument("-pre","--prefix", help="the prefix for all output files")
-parser.add_argument("-hco","--hco", help="the quickmerge hco parameter")
-parser.add_argument("-c","--c", help="the quickmerge c parameter")
-parser.add_argument("-l","--length_cutoff", help="minimum contig length to be merged")
+parser.add_argument("-hco","--hco", help="the quickmerge hco parameter (default=5.0)")
+parser.add_argument("-c","--c", help="the quickmerge c parameter (default=1.5)")
+parser.add_argument("-l","--length_cutoff", help="minimum contig length to be merged (default=0)")
 parser.add_argument("--no_nucmer", help="skip the nucmer step",action="store_true")
 parser.add_argument("--no_delta", help="skip the nucmer and delta-filter steps",action="store_true")
 parser.add_argument("--stop_after_nucmer", help="do not perform the delta-filter and merger steps",action="store_true")
@@ -27,10 +27,16 @@ hypath = args.hybrid_assembly_fasta
 selfpath = args.self_assembly_fasta
 if args.hco:
   hco = args.hco
+else:
+  hco = 5.0
 if args.c:
   c = args.c
+else:
+  c = 1.5
 if args.length_cutoff:
   length_cutoff = args.length_cutoff
+else:
+length_cutoff = 0
 
 #run nucmer:
 if not args.no_nucmer and not args.no_delta:
@@ -43,15 +49,15 @@ if not args.no_delta and not args.stop_after_nucmer:
 
 #append the correct options to the merger call:
 mergercall = ['quickmerge','-d',str(str(prefix)+'.rq.delta'),'-q',str(hypath),'-r',str(selfpath)]
-if args.hco:
-  mergercall.append('-hco')
-  mergercall.append(str(hco))
-if args.c:
-  mergercall.append('-c')
-  mergercall.append(str(c))
-if args.length_cutoff:
-  mergercall.append('-l')
-  mergercall.append(str(length_cutoff))
+#if args.hco:
+mergercall.append('-hco')
+mergercall.append(str(hco))
+#if args.c:
+mergercall.append('-c')
+mergercall.append(str(c))
+#if args.length_cutoff:
+mergercall.append('-l')
+mergercall.append(str(length_cutoff))
 
 #run the merging program
 if not args.stop_after_nucmer and not args.stop_after_df:
