ce29e56e02066dd896e09c372b3cbbfabeb43f66
jgbaldwinbrown <jbaldwi1@uci.edu>
2016-07-06 14:10:24 -0700

    Fixed readme and added header fixing to wrapper

--- merge_wrapper.py
+++ merge_wrapper.py
@@ -12,83 +12,146 @@ parser.add_argument("self_assembly_fasta",help="the output of a self assembly pr
 parser.add_argument("-pre","--prefix", help="the prefix for all output files")
 parser.add_argument("-hco","--hco", help="the quickmerge hco parameter (default=5.0)")
 parser.add_argument("-c","--c", help="the quickmerge c parameter (default=1.5)")
-parser.add_argument("-l","--length_cutoff", help="minimum contig length to be merged (default=0)")
+parser.add_argument("-l","--length_cutoff", help="minimum seed contig length to be merged (default=0)")
 parser.add_argument("--no_nucmer", help="skip the nucmer step",action="store_true")
 parser.add_argument("--no_delta", help="skip the nucmer and delta-filter steps",action="store_true")
 parser.add_argument("--stop_after_nucmer", help="do not perform the delta-filter and merger steps",action="store_true")
 parser.add_argument("--stop_after_df", help="do not perform the merger step",action="store_true")
+parser.add_argument("-lm", "--length_minimum", help="set the minimum alignment length necessary for use in quickmerge (default 0)")
+parser.add_argument("--clean_only", help="generate safe FASTA files for merging, but do not merge",action="store_true")
 
 args=parser.parse_args()
 if args.prefix:
-  prefix = args.prefix
+    prefix = args.prefix
 else:
-  prefix = "out"
+    prefix = "out"
 hypath = args.hybrid_assembly_fasta
 selfpath = args.self_assembly_fasta
 if args.hco:
-  hco = args.hco
+    hco = args.hco
 else:
-  hco = 5.0
+    hco = 5.0
 if args.c:
-  c = args.c
+    c = args.c
 else:
-  c = 1.5
+    c = 1.5
 if args.length_cutoff:
-  length_cutoff = args.length_cutoff
+    length_cutoff = args.length_cutoff
 else:
-  length_cutoff = 0
+    length_cutoff = 0
+if args.length_minimum:
+    length_minimum = args.length_minimum
+else:
+    length_minimum = 0
 
-#check for fasta compatibility:
+
+##define functions:
+def test_for_namedups(connection1,connection2):
+    ok1=True
+    ok2=True
+    names = set()
+    badnames = {}
+    for line in connection1:
+        if line[0] == ">":
+            line = line.rstrip('\n')
+            name = line[1:]
+            if not name in names:
+                names.add(name)
+            else:
+                ok1 = False
+                badnames[name] = 0
+    for line in connection2:
+        if line[0] == ">":
+            line = line.rstrip('\n')
+            name = line[1:]
+            if not name in names:
+                names.add(name)
+            else:
+                ok2 = False
+                badnames[name] = 0
+    return([ok1,ok2,names,badnames])
+
+def fix_namedup(name):
+    global badnames
+    if name not in badnames:
+        return(name)
+    else:
+        badnames[name] += 1
+        newname = str(name) + "." + str(badnames[name])
+        while newname in badnames or newname in names:
+            badnames[name] += 1
+            newname = str(name) + "." + str(badnames[name])
+        return(newname)
+
+#check for fasta header compatibility:
 testpaths = [str(hypath),str(selfpath)]
 order = ["hybrid","self"]
+
+a = test_for_namedups(open(testpaths[0],"r"),open(testpaths[1],"r"))
+ok1 = a[0]
+ok2 = a[1]
+names = a[2]
+badnames = a[3]
+
+#check for fasta line compatibility:
 for iteration in range(0,2):
-  file = testpaths[iteration]
-  with open(file,"r") as tfile:
-    i = 0
-    ok = True
-    for linea in tfile:
-      line = linea.rstrip('\n')
-      if i % 2 == 0 and line[0] != ">": ok = False
-      if " " in line: ok = False
-      i += 1
-    
-    #if fasta isn't compatible, make oneline version of fasta:
-    if not ok:
-      tfile.seek(0)
-      tempoutpath = order[iteration] + "_oneline.fa"
-      with open(tempoutpath,"w") as tout:
-        header = ""
-        seq = ""
+    file = testpaths[iteration]
+    with open(file,"r") as tfile:
+        i = 0
+        ok = True
         for linea in tfile:
-          line = linea.rstrip('\n')
-          #print line # debug line
-          if line[0] == ">":
-            #print line # debug line
-            if len(header) > 0 and len(seq) > 0:
-              tout.write(header + "\n" + seq + "\n")
-            if not " " in line:
-              header = line
+            line = linea.rstrip('\n')
+            if i % 2 == 0 and line[0] != ">": ok = False
+            if " " in line: ok = False
+            i += 1
+
+
+
+        #if fasta isn't compatible, make oneline version of fasta:
+        if not ok:
+            tfile.seek(0)
+            tempoutpath = order[iteration] + "_oneline.fa"
+            with open(tempoutpath,"w") as tout:
+                header = ""
+                seq = ""
+                for linea in tfile:
+                    line = linea.rstrip('\n')
+                    #print line # debug line
+                    if line[0] == ">":
+                        #print line # debug line
+                        if len(header) > 0 and len(seq) > 0:
+                            if not ok1 or not ok2:
+                                header_out = ">" + str(fix_namedup(header[1:]))
+                            else:
+                                header_out = header
+                            tout.write(header_out + "\n" + seq + "\n")
+                        if not " " in line:
+                            header = line
+                        else:
+                            header = line.split(" ")[0]
+                        seq = ""
+                    else:
+                        seq = seq + line
+                if not ok1 or not ok2:
+                    header_out = ">" + str(fix_namedup(header[1:]))
+                else:
+                    header_out = header
+                tout.write(header + "\n" + seq + "\n")
+            #change hypath and selfpath to use the temporary (oneline) fastas:
+            if iteration == 0:
+                hypath = tempoutpath
             else:
-              header = line.split(" ")[0]
-            seq = ""
-          else:
-            seq = seq + line
-        tout.write(header + "\n" + seq + "\n")
-      #change hypath and selfpath to use the temporary (oneline) fastas:
-      if iteration == 0:
-        hypath = tempoutpath
-      else:
-        selfpath = tempoutpath
-      
+                selfpath = tempoutpath
+            
 
 #run nucmer:
-if not args.no_nucmer and not args.no_delta:
-  subprocess.call(['nucmer','-l','100','-prefix',str(prefix),str(selfpath),str(hypath)])
+if not args.no_nucmer and not args.no_delta and not args.clean_only:
+    subprocess.call(['nucmer','-l','100','-prefix',str(prefix),str(selfpath),str(hypath)])
 
 #run the delta filter on the nucmer alignment:
-if not args.no_delta and not args.stop_after_nucmer:
-  with open(str(str(prefix)+'.rq.delta'), "w") as outfile:
-    subprocess.call(['delta-filter','-i','95','-r','-q',str(str(prefix)+'.delta')],stdout=outfile)
+if not args.no_delta and not args.stop_after_nucmer and not args.clean_only:
+    with open(str(str(prefix)+'.rq.delta'), "w") as outfile:
+        subprocess.call(['delta-filter','-i','95','-r','-q',str(str(prefix)+'.delta')],stdout=outfile)
 
 #append the correct options to the merger call:
 mergercall = ['quickmerge','-d',str(str(prefix)+'.rq.delta'),'-q',str(hypath),'-r',str(selfpath)]
@@ -101,8 +164,11 @@ mergercall.append(str(c))
 #if args.length_cutoff:
 mergercall.append('-l')
 mergercall.append(str(length_cutoff))
+#if args.length_minimum:
+mergercall.append('-lm')
+mergercall.append(str(length_minimum))
 
 #run the merging program
-if not args.stop_after_nucmer and not args.stop_after_df:
-  subprocess.call(mergercall)
+if not args.stop_after_nucmer and not args.stop_after_df and not args.clean_only:
+    subprocess.call(mergercall)
 
